ff46a77468f9b61b9cff7c88560806c2bbc0809d
Eric Molina <100007096.joan23@fje.edu>
2024-01-11 11:15:58 +0100

    fixed functionality and added animations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // ERIC MOLINA MOLINA - DAW 2
 
+var figuraSeleccionada = "";
 const figuras = {
     "triangulo": 2,
     "rectangulo": 2,
@@ -7,26 +8,44 @@ const figuras = {
     "circulo": 1
 }
 
-window.onload = function () {
-    document.getElementById("submit").onclick = CargarFigura;
-    document.getElementById("clear").onclick = ClearFigura;
+window.onload = function() {
+    document.getElementById('lado1').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            CargarFigura();
+        }
+    });
+    
+    document.getElementById('lado2').addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            CargarFigura();
+        }
+    });
 }
 
 function CargarLadosForm(figura) {
-    figuras_container.style.display = "none";
-    lados_container.style.display = "inline-block";
+    figuraSeleccionada = figura;
+    
 
     lado1.style.display = "inline-block";
     lado1_.style.display = "inline-block";
-    lado2.style.display = figuras[figura] == 2 ? "inline-block" : "none";
-    lado2_.style.display = figuras[figura] == 2 ? "inline-block" : "none";
-    document.getElementsByClassName("lados_margin")[0].style.display = figuras[figura] == 2 ? "none" : "inline-block";
+    lado2.style.display = figuras[figuraSeleccionada] == 2 ? "inline-block" : "none";
+    lado2_.style.display = figuras[figuraSeleccionada] == 2 ? "inline-block" : "none";
+    document.getElementsByClassName("lados_margin")[0].style.display = figuras[figuraSeleccionada] == 2 ? "none" : "inline-block";
+
+    //Transicion: figuras -> lados
+
+    document.getElementById("figuras_container").classList.remove('in_anim');
+    document.getElementById("figuras_container").classList.add('out_anim');
+
+    document.getElementById("lados_container").classList.remove('initial_display_none');
+    document.getElementById("lados_container").classList.remove('out_anim');
+    document.getElementById("lados_container").classList.add('in_anim');
+
+    //document.getElementById("figuras_container").style.display = "none";
+    //document.getElementById("lados_container").style.display = "inline-block";
 }
 
 function CargarFigura() {
-
-    const desplegable = document.getElementById(desplegableID);
-    const tipoFigura = desplegable.value;
     const valorLado1 = document.getElementById("lado1").value;
     const valorLado2 = document.getElementById("lado2").value;
 
@@ -34,15 +53,28 @@ function CargarFigura() {
     ajax.open('POST', './proc/calcular.php');
 
     var formData = new FormData();
-    formData.append('tipoFigura', tipoFigura);
+    formData.append('tipoFigura', figuraSeleccionada);
     formData.append('valorLado1', valorLado1);
     formData.append('valorLado2', valorLado2);
 
     ajax.onload = function () {
         if (ajax.status == 200) {
             document.getElementById("result").innerHTML = ajax.responseText;
-            document.getElementById("result").style.display = "inline-block";
-            document.getElementById("result_container").style.display = "inline-block";
+
+            resultado_texto_lado2.style.display = figuras[figuraSeleccionada] == 2 ? "inline-block" : "none";
+            document.getElementsByClassName("resultado_margin")[0].style.display = figuras[figuraSeleccionada] == 2 ? "none" : "inline-block";
+
+            //Transicion: lados -> resultado
+
+            document.getElementById("lados_container").classList.remove('in_anim');
+            document.getElementById("lados_container").classList.add('out_anim');
+
+            document.getElementById("resultado_container").classList.remove('initial_display_none');
+            document.getElementById("resultado_container").classList.remove('out_anim');
+            document.getElementById("resultado_container").classList.add('in_anim');
+
+            //document.getElementById("lados_container").style.display = "none";
+            //document.getElementById("resultado_container").style.display = "inline-block";
 
         } else {
             alert("algo ha ido mal");
@@ -53,9 +85,19 @@ function CargarFigura() {
 }
 
 function ClearFigura() {
-    document.getElementById("lado1").value = "";
-    document.getElementById("lado2").value = "";
-    document.getElementById("result").innerHTML = "";
-    document.getElementById("result").style.display = "none";
-    document.getElementById("result_container").style.display = "none";
+
+    document.getElementById("lado1").value = 0;
+    document.getElementById("lado2").value = 0;
+
+    //Transicion: resultado -> figuras
+
+    document.getElementById("resultado_container").classList.remove('in_anim');
+    document.getElementById("resultado_container").classList.add('out_anim');
+
+    document.getElementById("figuras_container").classList.remove('out_anim');
+    document.getElementById("figuras_container").classList.add('in_anim');
+
+
+    //document.getElementById("resultado_container").style.display = "none";
+    //document.getElementById("figuras_container").style.display = "inline-block";
 }
--- proc/calcular.php
+++ proc/calcular.php
@@ -21,8 +21,24 @@ if ($_POST["tipoFigura"] == "triangulo") {
 } else {
     exit();
 }
-
-echo '<p>Tipo figura: '.$figura->GetTipoFigura().'</p>';
-echo '<p>Área: '.round($figura->Area(), 2).'</p>';
-echo '<p>Perímetro: '.round($figura->Perimetro(), 2).'</p>';
+$lado2 = isset($figura->lado2) ? $figura->lado2 : 0;
+echo '<div id="resultado_titulo">
+    <button type="button" onclick="ClearFigura()">VOLVER</button>
+</div>
+<div id="resultado_figura">
+    <img src="./img/'.$figura->GetTipoFigura().'.png" alt="figura">
+</div>
+<div class="resultado_margin"></div>
+<div class="resultado_texto">
+    <p>Lado 1: '.$figura->lado1.'</p>
+</div>
+<div class="resultado_texto" id="resultado_texto_lado2">
+    <p>Lado 2: '.$lado2.'</p>
+</div>
+<div class="resultado_texto">
+    <p>Área: '.round($figura->Area(), 2).'</p>
+</div>
+<div class="resultado_texto">
+    <p>Perímetro: '.round($figura->Perimetro(), 2).'</p>
+</div>';
 ?>
